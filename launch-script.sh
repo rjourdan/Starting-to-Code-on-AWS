f688afee049df95022dd2bf48d5629ff15cffb2b
rjourdan <romjour@amazon.com>
2025-09-28 14:39:27 +0000

    FIx: include the Bitnami node path in the service environment

--- launch-script.sh
+++ launch-script.sh
@@ -15,6 +15,7 @@ ConditionPathExists=!/var/lib/remarket-setup-complete
 Type=oneshot
 User=root
 Environment=DB_PASSWORD=postgres
+Environment=PATH=/opt/bitnami/node/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 ExecStart=/bin/bash -c 'sudo mkdir -p /opt/bitnami/projects && cd /opt/bitnami/projects && curl -o install-script.sh https://raw.githubusercontent.com/rjourdan/Starting-to-Code-on-AWS/step1-monolith-Lightsail/install-script.sh && chmod +x install-script.sh && ./install-script.sh && touch /var/lib/remarket-setup-complete'
 RemainAfterExit=yes
 StandardOutput=journal
